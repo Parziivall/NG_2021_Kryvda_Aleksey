b1c17d7a80249a3b0d42dfb3e5297afcd0ed9b94
Parziivall <lesha020103@gmail.com>
2021-10-24 12:58:27 +0300

    Add task1

--- lesson 1/task3/main.cpp
+++ lesson 1/task3/main.cpp
@@ -6,10 +6,8 @@ int main()
 {
     int a = 50;
     int b = 20;
-    a = a + b;
+
     b = a - b;
-    a = a - b;
-    cout << "a - " << a << endl;
-    cout << "b - " << b << endl;
-    return 0;
+    cin >> b;
+    return 0
 }
---
+++ lesson 2/task1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Enter third number: ";
+    cin >> c;
+    cout << "Enter four number: ";
+    cin >> d;
+
+    // a + b + c + d = e
+    e = a + b + c + d;
+    cout << "e = " << e << endl;
+    return 0;
+}
