8bae2b85b29183dbd63b5da052000bdb6ba9bf18
Parziivall <lesha020103@gmail.com>
2021-10-27 18:33:51 +0300

    Add task5

---
+++ lesson 1/task5/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m;
+    cout << "Enter mass of spaceship: ";
+    cin >> m;
+    cout << "Fuel need to fly is - " << (m/3) - 2 << "l" << endl;
+    return 0;
+}
