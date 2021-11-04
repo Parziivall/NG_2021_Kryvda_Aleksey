39a779c83c91b1dc98bda039035e12eff47b6cdc
Parziivall <lesha020103@gmail.com>
2021-11-04 15:47:30 +0200

    task1 fixed

--- lesson 2/task1/main.cpp
+++ lesson 2/task1/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
     int n = 0;
-	int r = 0;
+    int r = 0;
     cout << "Enter your number: ";
     cin >> n;
     for(int i=n; i>0; i/=10)
 	{
-			r +=i%10;
+	    r +=i%10;
 	}
     cout << "The sum of all numbers is: " << r << endl;
     return 0;
